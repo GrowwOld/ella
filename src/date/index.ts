4ff1933fd7121bf1ac3cab04c6607c71c7cfa6e8
Vikas Singh <vikassingh@Groww-Admins-MacBook-Pro.local>
2022-01-22 14:58:43 +0530

    some new methods added and removed docs from npmignore

--- src/date/index.ts
+++ src/date/index.ts
@@ -1,22 +1,23 @@
 /**
- * Generate a random string of any length.
- * @param {number} stringLength - The title of the book.
+ * This method can be used to get month name abbreviation from month number.
+ *
+ * @param {number} monthNumber - Month number, eg: for Jan, it is 1
+ *
+ * @remarks
+ * monthNumber should be in the range of 1-12
+ *
+ * @example
+ * ```
+ * getMonthAbbrByIndex(2) // Feb will be the output
+ * ```
+ *
+ * @category Date Based Method
  */
-export function readableRandomStringMaker(stringLength:number) {
-  let randomString = '';
+export function getMonthAbbrByIndex(monthNumber:number):string {
+  const monthNames = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
 
-  while (randomString.length < stringLength) {
-    randomString += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.charAt(Math.random() * 62 | 0);
-  }
-
-  return randomString;
-}
-
-/**
- * This method is used to generate random number between any 2 numbers both inclusive
- * @param {number} arg1 - First number of generation range inclusive.
- * @param {number} arg2 - Second number of generation range inclusive.
- */
-export function generateRandomNumber(arg1:number, arg2:number):number {
-  return arg1 + arg2;
+  return monthNames[ monthNumber - 1 ];
 }
--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,3 +1,82 @@
-export function subtract(arg1:number, arg2:number):number {
-  return arg1 - arg2;
+import { isEmpty } from "../general";
+
+/**
+ * This method can be used to listen any custom event.
+ *
+ * @param {string} eventName - Name of the event that you want to listen or subscribe
+ * @param {Function} callback - Callback function which will be called upon dispatching of that event
+ *
+ * @remarks
+ * It's strongly recommended to add the event name in utils/constants/CUSTOM_EVENTS and use. No literals allowed.
+ *
+ * @example
+ * ```
+ * listenToCustomEvent(CUSTOM_EVENTS.file_loaded, this.xyz)
+ * ```
+ *
+ * @category DOM Based Method
+ */
+export function listenToCustomEvent(eventName:string, callback:Function) {
+  try {
+    if (!isEmpty(document)) {
+      document.documentElement.addEventListener(eventName, callback as EventListener);
+    }
+
+  } catch (error) {
+    console.error(`Error in listening to ${eventName} custom event: `, error);
+  }
+}
+
+
+/**
+ * This method can be used to dispatch any custom event.
+ *
+ * @param {string} eventName - Name of the event that you want to dispatch
+ * @param {object} eventDetails - OPTIONAL param, eventDetails object can be used to pass extra details
+ *
+ * @remarks
+ * It's strongly recommended to add the event name in utils/constants/CUSTOM_EVENTS and use. No literals allowed.
+ *
+ * @example
+ * ```
+ * dispatchCustomEvent(CUSTOM_EVENTS.file_loaded)
+ * dispatchCustomEvent(CUSTOM_EVENTS.file_loaded, {user: 'Ella'})
+ * ```
+ *
+ * @category DOM Based Method
+ */
+export function dispatchCustomEvent(eventName: string, eventDetails:object = {}) {
+  if (!isEmpty(document)) {
+    const temp = document.documentElement;
+
+    temp.dispatchEvent(new CustomEvent(eventName, { detail: eventDetails }));
+  }
+}
+
+
+/**
+ * This method can be used to unlisten any custom event.
+ *
+ * @param {string} eventName - Name of the event that you want to unlisten or unsubscribe
+ * @param {Function} methodToUnlisten - The EventListener function of the event handler to remove from the event target.
+ *
+ * @remarks
+ * It's strongly recommended to add the event name in utils/constants/CUSTOM_EVENTS and use. No literals allowed.
+ *
+ * @example
+ * ```
+ * unlistenToCustomEvent(CUSTOM_EVENTS.file_loaded, this.xyz)
+ * ```
+ *
+ * @category DOM Based Method
+ */
+export function unlistenToCustomEvent(eventName:string, methodToUnlisten:Function) {
+  try {
+    if (!isEmpty(document)) {
+      document.documentElement.removeEventListener(eventName, methodToUnlisten as EventListener);
+    }
+
+  } catch (error) {
+    console.error(` Error in unListening to ${eventName} custom event: `, error);
+  }
 }
